260150c3e5db86d2398ac63a106a4eb103a6f94b
themediocrebritain <jacob.dawson@me.com>
2017-09-09 15:25:53 -0400

    the real files for this branch

--- csvtocust.py
+++ csvtocust.py
@@ -1,72 +1,181 @@
 import os
 
-def shorten_info (inpath, outpath):
-	#print(inpath)
-	time=''
-	note=''
-	outfile = open(outpath,'w') #creates a new file for output
-	
-	with open(inpath) as f: #tbh im not reaaaally sure that this line is necessary, but i dont wanna risk it
+def meganify(inTim):
+	if inTim == '30':
+		return'a'
+	elif inTim == '31':
+		return'b'
+	elif inTim == '32':
+		return'c'
+	elif inTim == '33':
+		return'd'
+	elif inTim == '34':
+		return'e'
+	elif inTim == '35':
+		return'f'
+	elif inTim == '36':
+		return'g'
+	elif inTim == '37':
+		return'h'
+	elif inTim == '38':
+		return'i'
+	elif inTim == '39':
+		return'j'
+	elif inTim == '40':
+		return'k'
+	elif inTim == '41':
+		return'l'
+	elif inTim == '42':
+		return'm'
+	elif inTim == '43':
+		return'n'
+	elif inTim == '44':
+		return'o'
+	elif inTim == '45':
+		return'p'
+	elif inTim == '46':
+		return'q'
+	elif inTim == '47':
+		return'r'
+	elif inTim == '48':
+		return's'
+	elif inTim == '49':
+		return't'
+	elif inTim == '50':
+		return'u'
+	elif inTim == '51':
+		return'v'
+	elif inTim == '52':
+		return'w'
+	elif inTim == '53':
+		return'x'
+	elif inTim == '54':
+		return'y'
+	elif inTim == '55':
+		return'z'
+	elif inTim == '56':
+		return','
+	elif inTim == '57':
+		return'.'
+	elif inTim == '58':
+		return';'
+	elif inTim == '59':
+		return'`'
+	elif inTim == '60':
+		#print('just letting you know, im workinggggggggggggggggggggggggggggggggggggggggggggggggggggggg')
+		return'A'
+	elif inTim == '61':
+		return'B'
+	elif inTim == '62':
+		return'C'
+	elif inTim == '63':
+		return'D'
+	elif inTim == '64':
+		return'E'
+	elif inTim == '65':
+		return'F'
+	elif inTim == '66':
+		return'G'
+	elif inTim == '67':
+		return'H'
+	elif inTim == '68':
+		return'I'
+	elif inTim == '69':
+		return'J'
+	elif inTim == '70':
+		return'K'
+	elif inTim == '71':
+		return'L'
+	elif inTim == '72':
+		return'M'
+	elif inTim == '73':
+		return'N'
+	elif inTim == '74':
+		return'O'
+	elif inTim == '75':
+		return'P'
+	elif inTim == '76':
+		return'Q'
+	elif inTim == '77':
+		return'R'
+	elif inTim == '78':
+		return'S'
+	elif inTim == '79':
+		return'T'
+	elif inTim == '80':
+		return'U'
+	elif inTim == '81':
+		return'V'
+	elif inTim == '82':
+		return'W'
+	elif inTim == '83':
+		return'X'
+	elif inTim == '84':
+		return'Y'
+	elif inTim == '85':
+		return'Z'
+	elif inTim == '86':
+		return'<'
+	elif inTim == '87':
+		return'>'
+	elif inTim == '88':
+		return':'
+	elif inTim == '89':
+		return'~'
+	elif inTime == '90':
+		return'_'
+	else:
+		#print('jacob you coded wrong')
+		return''
+
+def customize (inpath, outpath):
+
+	CSVnote = ''
+	CUSTnote = ''
+	time = ''
+	greatest_time = '-1'
+	timeNumber = 0
+	goddammitwhywontyouiterateoverintegers = 0
+	seriouslyjustiterateoverintegers = 0
+	number_of_spaces = 0
+	lastLine = '2, 0, Note_on_c, 0, 79, 100'
+
+	justforthelols = open(outpath,'w')
+	#makes/clears a new file in the folder outcust
+	justforthelols.close()
+
+	with open(inpath) as jkl:
 
 		lines = [line.rstrip('\n') for line in open(inpath)]
+	with open(outpath,'a') as f:
 
-		for i in lines:
-			#print(i[-21:-12])
-			if (i[-3:] == '100') & (i[-21:-12] == 'Note_on_c'): #so now we're only reading lines with notes
-				
-				#print('working')
-				time = i[3:-23] #this is the first thing we care about
-				note = i [-11:-5] #this is the only other thing we care about. Seriously, if it's written like this csv, it has no other useful information.
-				#print(time + note) #so we can see whats goign on
-				f = open(outpath, 'a')
-				f.write(time+note+'\n') #should output the same as the print() function
-				f.close()
+		for e in lines:
+			#print(e[-21:-12])
+			if (e[-3:] == '100') & (e[-21:-12] == 'Note_on_c'):
 
-#which is great and all, but I gotta make a new file structure that plays the notes at the same time. Y'know, so it sounds right.
-#unless, of course, the neural net learns to count up whenever it makes a new line.
-#who knows.
+				time = e[3:-23]
+				#print(time)
+				if (int(greatest_time) < int(time)):
+					greatest_time = time
+		print('greatest time found: '+greatest_time)
+		#this whole block of code should just
+		#find the highest time in the piece
 
-def normalize_time(inpath, outpath): #note to self: if this screws up, change the names of all these variables
-	time = ''
-	note = ''
-	outfile = open(outpath,'w')
+		for line in lines:
+			if (line[-21:-12] == 'Note_on_c') & (line[-3:] == '100'):
+				number_of_spaces  = int(line[3:-23]) - int(lastLine[3:-23])
+				#print(int(number_of_spaces))
+				seriouslyjustiterateoverintegers=0
+				while seriouslyjustiterateoverintegers < number_of_spaces:
+					#thing
+					f.write(meganify(line[-7:-5])+'\n')
+					#print(meganify(line[-7:-5]))
+					seriouslyjustiterateoverintegers += 1
+				#f.write(meganify(line[-7:-5])+'\n')
+				#print(line[-7:-5])
+				lastLine = line
+				#print(lastLine)
 
-	lines = [line.rstrip('\n') for line in open(inpath)] #maybe unnecessary? we'll see.
-	
-	#ok so now, whereever in 'times' the number matches, I wanna have all the notes record to the same line.
-	#note, I will not enjoy converting this back into a csv.
-
-	last_i = ''
-	for i in lines:
-		if (last_i != '') & (i[:-6] == last_i[:-6]):
-			
-			#get rid of the last line of stuff in the outfile before we add the new two notes
-
-			with open(outpath,'r') as q:
-				foo = q.readlines()
-			foo = foo[:-1]
-
-			#moarvariables = open(outpath,'w')
-			#moarvariables.close()
-
-			with open(outpath, 'r+') as bar:
-				for foobar in foo:
-					bar.write(foobar)
-			#i know i know im a bad coder yeah yeah
-
-			f = open(outpath, 'a')
-			f.write(last_i[-5:] + i[-6:] + '\n')
-			f.close()
-		else:
-			#in case it's a whole new time
-			with open(outpath,'a') as g:
-				g.write(i[-5:] + '\n')
-		last_i = i
-
-
-for j in os.listdir('incsv'):
-	print('incsv/'+j + ' written into ' + j[:-4] + '.cust')
-	shorten_info('incsv/' + j, 'outsemi/' + j[:-4] + '.cust')
-for k in os.listdir('outsemi'):
-	print('outsemi/'+k + ' written into outcsv/' + k)
-	normalize_time('outsemi/' + k, 'outcust/' + k)
+for q in os.listdir('incsv'):
+	print(q + ' written into outcust/'+q[:-3]+'cust')
+	customize('incsv/'+ q , 'outcust/'+q[:-3]+'cust')
--- custtocsv.py
+++ custtocsv.py
@@ -1,55 +1,177 @@
 import os
 
-def return_to_csv(infile,outfile):
-	time = 0
-	time_increment = 200
-	opening_text = """0, 0, Header, 1, 2, 480
+def convertBackToGoofyNumberSystem(note):
+	if (note == 'a'):
+		return '30'
+	elif (note == 'b'):
+		return '31'
+	elif (note == 'c'):
+		return '32'
+	elif (note == 'd'):
+		return '33'
+	elif (note == 'e'):
+		return '34'
+	elif (note == 'f'):
+		return '35'
+	elif (note == 'g'):
+		return '36'
+	elif (note == 'h'):
+		return '37'
+	elif (note == 'i'):
+		return '38'
+	elif (note == 'j'):
+		return '39'
+	elif (note == 'k'):
+		return '40'
+	elif (note == 'l'):
+		return '41'
+	elif (note == 'm'):
+		return '42'
+	elif (note == 'n'):
+		return '43'
+	elif (note == 'o'):
+		return '44'
+	elif (note == 'p'):
+		return '45'
+	elif (note == 'q'):
+		return '46'
+	elif (note == 'r'):
+		return '47'
+	elif (note == 's'):
+		return '48'
+	elif (note == 't'):
+		return '49'
+	elif (note == 'u'):
+		return '50'
+	elif (note == 'v'):
+		return '51'
+	elif (note == 'w'):
+		return '52'
+	elif (note == 'x'):
+		return '53'
+	elif (note == 'y'):
+		return '54'
+	elif (note == 'z'):
+		return '55'
+	elif (note == ','):
+		return '56'
+	elif (note == '.'):
+		return '57'
+	elif (note == ';'):
+		return '58'
+	elif (note == '`'):
+		return '59'
+	elif (note == 'A'):
+		return '60'
+	elif (note == 'B'):
+		return '61'
+	elif (note == 'C'):
+		return '62'
+	elif (note == 'D'):
+		return '63'
+	elif (note == 'E'):
+		return '64'
+	elif (note == 'F'):
+		return '65'
+	elif (note == 'G'):
+		return '66'
+	elif (note == 'H'):
+		return '67'
+	elif (note == 'I'):
+		return '68'
+	elif (note == 'J'):
+		return '69'
+	elif (note == 'K'):
+		return '70'
+	elif (note == 'L'):
+		return '71'
+	elif (note == 'M'):
+		return '72'
+	elif (note == 'N'):
+		return '73'
+	elif (note == 'O'):
+		return '74'
+	elif (note == 'P'):
+		return '75'
+	elif (note == 'Q'):
+		return '76'
+	elif (note == 'R'):
+		return '77'
+	elif (note == 'S'):
+		return '78'
+	elif (note == 'T'):
+		return '79'
+	elif (note == 'U'):
+		return '80'
+	elif (note == 'V'):
+		return '81'
+	elif (note == 'W'):
+		return '82'
+	elif (note == 'X'):
+		return '83'
+	elif (note == 'Y'):
+		return '84'
+	elif (note == 'Z'):
+		return '85'
+	elif (note == '<'):
+		return '86'
+	elif (note == '>'):
+		return '87'
+	elif (note == ':'):
+		return '88'
+	elif (note == '~'):
+		return '89'
+	elif (note == '_'):
+		return '90'
+	else:
+		return '60'
+		print('JACOB YOU CODED THE CONVERTION CHECK WRONG NOOOOOOOOOOOOOOO')
+	#et cetera, you need to hardcode these later jacob
+
+def returnToCSV (infile,outpath):
+
+	startThingy = '''0, 0, Header, 1, 2, 480
 1, 0, Start_track
 1, 0, Title_t, "Close Encounters"
 1, 0, Text_t, "Sample for MIDIcsv Distribution"
 1, 0, Copyright_t, "This file is in the public domain"
 1, 0, Time_signature, 4, 2, 24, 8
-1, 0, Tempo, 500000
+1, 0, Tempo, 1200000
 1, 0, End_track
 2, 0, Start_track
 2, 0, Instrument_name_t, "Solo Lute or Keyboard"
-2, 0, Program_c, 0, 19
-"""
+2, 0, Program_c, 0, 19''' + '\n'
+
+	endThingy = ''', End_track
+0, 0, End_of_file'''
+
+	time = 0
 	lastLine = ''
-	with open(outfile,'w') as x:
-		x.write(opening_text)
 
-	lines = [line.rstrip('\n') for line in open(infile)]
+	heregoessometext = open(outpath, 'w')
+	heregoessometext.write(startThingy)
+	heregoessometext.close()
 
-	for i in lines:
-		time += time_increment
-		#print(len(i))
-		if len(i) == 5:
-			with open(outfile,'a') as f:
-				f.write('2, '+str(time)+', Note_on_c, '+lastLine+''', 100
-''')
-				f.write('2, '+str(time)+', Note_on_c, '+i+''', 100
-''')
-		elif len(i) == 11:
-			with open(outfile,'a') as f:
-				#print('2, '+str(time)+', Note_on_c, '+i[5:]+', 100\n')
-				#print('2, '+str(time)+', Note_on_c, '+i[:5]+', 100\n')
-				f.write('2, '+str(time)+', Note_on_c,'+lastLine[5:]+''', 100
-''')
-				f.write('2, '+str(time)+', Note_on_c,'+i[5:]+''', 100
-''')	#maybe now it will be windows compliant? -update: No.
-	#but it still kinda works though? theres something weird going on at the end i need to fix?
-				f.write('2, '+str(time)+', Note_on_c, '+lastLine[:5]+''', 100
-''')
-				f.write('2, '+str(time)+', Note_on_c, '+i[:5]+''', 100
-''')
-		lastLine = i
-	with open(outfile,'a') as jkl:
-		time += time_increment
-		jkl.write('''2, '''+str(time)+''', End_track
-0, 0, End_of_file
-''')
+	with open(infile) as asdfghjkl:
+		lines = [line.rstrip('\n') for line in asdfghjkl]
+
+	lastLine = lines[0]
+
+	with open(outpath,'a') as f:
+		#god this is gonna be weird to algorithmize
+		for line in lines:
+			if (line != lastLine):
+				#print(line)
+				#print(lastLine)
+				#print('changes at '+str(time))
+				f.write('2, '+str(time)+', Note_on_c, 0, '+convertBackToGoofyNumberSystem(lastLine)+', 0'+'\n')
+				f.write('2, '+str(time)+', Note_on_c, 0, '+convertBackToGoofyNumberSystem(line)+', 100'+'\n')
+			#print(str(time))
+			
+			time += 1
+			lastLine = line
+		f.write('2, ' + str(time) + endThingy)
 
 for i in os.listdir('incust'):
-	print('incust/'+i+' written into outcsv/'+i[:-4]+'csv')
-	return_to_csv('incust/'+i,'outcsv/'+i[:-4]+'csv')
+	print(i+' printed into outcsv/'+i[:-4]+'csv')
+	returnToCSV('incust/'+i,'outcsv/'+i[:-4]+'csv')
